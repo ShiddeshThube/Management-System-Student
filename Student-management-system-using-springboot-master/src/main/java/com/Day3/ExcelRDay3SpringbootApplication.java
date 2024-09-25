fc1e70c148b07dcd1c76e2f31ad73316ae76e370
ShiddeshThube <shiddeshthube8080@gmail.com>
2024-09-25 12:27:47 +0530

    first committt

---
+++ Student-management-system-using-springboot-master/src/main/java/com/Day3/ExcelRDay3SpringbootApplication.java
@@ -0,0 +1,16 @@
+package com.Day3;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class ExcelRDay3SpringbootApplication  {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcelRDay3SpringbootApplication.class, args);
+	}
+
+}
